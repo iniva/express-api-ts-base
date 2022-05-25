038a0a64f38cf93ea2f726d48b3476df09bb1b66
Tony Di Benedetto <tonyblast@gmail.com>
2022-05-25 11:57:34 +0200

    Update deps (#5)
    * chore: update deps.

--- docker/local/run.sh
+++ docker/local/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker-compose -p express-ts-api up --build --abort-on-container-exit
+DOCKER_BUILDKIT=1 docker compose -p express-ts-api up --build --abort-on-container-exit
